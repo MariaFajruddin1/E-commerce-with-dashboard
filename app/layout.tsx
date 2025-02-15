1c3d60929931ddf7f9ea4f0b4a29c0da021d4a88
M Bilal Hadid <110343359+MariaFajruddin1@users.noreply.github.com>
2025-02-16 02:54:01 +0500

    product add form complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+
+import { ModalProvider } from "@/providers/modal-provider";
+
 import "./globals.css";
 
 
@@ -29,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ModalProvider/>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
+"use client";
+
+import { useStoreModel } from "@/hooks/use-store-model";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Home() {
+  const onOpen = useStoreModel((state) => state.onOpen);
+  const isOpen = useStoreModel((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -15,6 +27,7 @@ export default function Home() {
           </button>
         </Link>
       </main>
+      <div>Root Page</div>
     </div>
   );
 }
